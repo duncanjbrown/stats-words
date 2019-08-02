b12e6346625c138b2ddff77c3e63249eaec71cc2
Duncan Brown <duncanjbrown@gmail.com>
2019-08-02 21:59:43 +0100

    Add :release mode to :app builds
    This mode
    
    - changes the output-dir to dist/js
    - adds a build hook to copy index.html out of public/ and into dist/
    - makes index.html refer to a relative path so it can be hosted in a
    subdirectory

--- src/dev/stats_words/tools.clj
+++ src/dev/stats_words/tools.clj
@@ -1,10 +1,15 @@
 (ns stats-words.tools
-  (:require [shadow.cljs.devtools.api :as shadow]))
+  (:require [shadow.cljs.devtools.api :as shadow]
+            [clojure.java.io :as io]))
 
 (defn start-repl! []
-  (shadow.cljs.devtools.server/start!)
   (shadow.cljs.devtools.api/watch :test)
   (shadow.cljs.devtools.api/watch :app)
   (shadow.cljs.devtools.api/nrepl-select :app))
 
-(start-repl!)
+(defn copy-assets
+  {:shadow.build/stage :flush}
+  [build-state & args]
+  (println "Copying public/index.html to dist/index.html")
+  (io/copy (io/file "public/index.html") (io/file "dist/index.html"))
+  build-state)
