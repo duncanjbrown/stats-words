0ab2a89fbe5f1871f1e30a48fe7179e17396ad2d
Duncan Brown <duncanjbrown@gmail.com>
2019-08-06 00:44:37 +0100

    Ignore blank lines

--- src/main/stats_words/transform.cljs
+++ src/main/stats_words/transform.cljs
@@ -1,8 +1,10 @@
-(ns stats-words.transform)
+(ns stats-words.transform
+  (:require [clojure.string :as s]))
 
 (defn shuffle-lines [input]
   (->> input
        (remove nil?)
+       (remove s/blank?)
        shuffle))
 
 (defn create-verses [verse-count line-count input]
